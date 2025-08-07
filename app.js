42d81ed62e7584284bbf70174b9eca7140957021
Tanaka <twekwete1@gmail.com>
2025-08-07 11:04:10 +0200

    clean ups

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import express from "express";
 import session from "express-session";
 import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
-import { findUserById, findUserByEmail } from "./controllers/user-controller.js";
 import { authRouter } from './routes/auth.js';
 
 const app = express();
@@ -23,62 +21,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await findUserById(id);
-    if (!user) {
-      return done(null, false);
-    }
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-
-passport.use(
-  new LocalStrategy({ usernameField: "email" }, async function (
-    email,
-    password,
-    cb
-  ) {
-    try {
-      const user = await findUserByEmail(email);
-
-      if (!user) {
-        return cb(null, false);
-      }
-
-      if (user.password != password) {
-        return cb(null, false);
-      }
-
-      return cb(null, user);
-    } catch (err) {
-      return cb(err);
-    }
-  })
-);
-
 app.use('/auth', authRouter);
-
-app.post("/login", passport.authenticate("local"), (req, res) => {
-  res.status(200).send("Login successful");
-  console.log(req.session)
-});
-
-
-app.post("/logout", (req, res, next) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.status(200).send("Logout successful");
-  });
-});
 
 
 app.listen(PORT, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/auth-controller.js";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { findUserByEmail,findUserById } from "../controllers/user-controller.js";
 
 export const authRouter = express.Router();
 
@@ -10,4 +13,59 @@ authRouter.post("/register", async (req, res) => {
   } catch (err) {
     res.status(400).json({"User registered failed":err.message})
   }
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await findUserById(id);
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    cb
+  ) {
+    try {
+      const user = await findUserByEmail(email);
+
+      if (!user) {
+        return cb(null, false);
+      }
+
+      if (user.password != password) {
+        return cb(null, false);
+      }
+
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
+  })
+);
+
+ authRouter.post("/login", passport.authenticate("local"), (req, res) => {
+  res.status(200).send("Login successful");
+  console.log(req.session)
+});
+
+
+ authRouter.post("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send("Logout successful");
+  });
 });
